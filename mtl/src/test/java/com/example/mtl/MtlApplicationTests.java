aae37c96aa45f37e6d5fc992cd74273c7d5b22de
WangTao <1609832760@qq.com>
2023-10-26 08:46:26 +0800

    二手回收平台

---
+++ mtl/src/test/java/com/example/mtl/MtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
